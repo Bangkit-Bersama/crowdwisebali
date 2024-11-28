e6b3602344867e02fbff48b6bb1679b2a2f0a73d
virasare <virasare@gmail.com>
2024-11-28 20:07:16 +0800

    update: auth interface

--- app/src/main/java/com/bangkit/crowdwisebali/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/bangkit/crowdwisebali/ui/auth/login/LoginActivity.kt
@@ -1,11 +1,7 @@
 package com.bangkit.crowdwisebali.ui.auth.login
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.bangkit.crowdwisebali.R
 import com.bangkit.crowdwisebali.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
