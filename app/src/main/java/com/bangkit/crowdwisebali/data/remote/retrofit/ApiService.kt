552d59eac11257cdae73eecd99281830be27e49d
virasare <virasare@gmail.com>
2024-11-30 02:19:17 +0800

    feat: sucessfully call the recommendation api

--- app/src/main/java/com/bangkit/crowdwisebali/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/bangkit/crowdwisebali/data/remote/retrofit/ApiService.kt
@@ -1,4 +1,28 @@
 package com.bangkit.crowdwisebali.data.remote.retrofit
 
-class ApiService {
+import com.bangkit.crowdwisebali.data.remote.response.RecommendationResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+
+    @GET("recommendation")
+    @Headers("Content-Type: application/json")
+    fun getRecommendation(
+        @Query("latitude") latitude: Double,
+        @Query("longitude") longitude: Double,
+        @Query("place_type") placeType: String
+    ): Call<RecommendationResponse>
+
+//    @GET("recommendation")
+//    suspend fun getRecommendation(
+//        @Body requestBody: Map<String, Any> // Body dalam format JSON
+//    ): RecommendationResponse
+//
+//    @GET("recommendation")
+//    suspend fun getRecommendation(
+//        @Query("latitude") latitude: Double,
+//        @Query("longitude") longitude: Double,
+//        @Query("place_type") placeType: String
+//    ): RecommendationResponse
 }
